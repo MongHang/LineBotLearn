c7959fa920c7beaeb44f325b3ca1494f52192f1c
MongHang <108526434+MongHang@users.noreply.github.com>
2023-08-08 21:53:10 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,40 +1,58 @@
-# LineBot所需套件
-from line_bar_api import *
+from line_bot_api import *
 from events.basic import *
+from events.oil import *
 
 app = Flask(__name__)
 
-# 監聽所有來自 /callback 的 Post Request
-@app.route("/callback", methods=["POST"])
+
+@handler.add(MessageEvent, message=TextMessage)
+def handl_message(event):
+    profile = line_bot_api.get_profile(event.source.user_id)
+    uid = profile.user_id
+    message_text = str(event.message.text).lower()
+
+    if message_text == '@使用說明':
+        about_us_event(event)
+        Usage(event)
+    if message_text == '想知道油價':
+        content = oil_price()
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=content)
+        )
+    if message_text == '股價查詢':
+        line_bot_api.push_message(
+            uid,
+            TextSendMessage("請輸入'#' + '股票代號'\n範例：#2330")
+        )
+    if message_text == '@小幫手':
+        popbar(event)
+@handler.add(FollowEvent)
+def handle_follow(event):
+    welcome_msg = '''Hello! 歡迎加入財經小幫手'''
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=welcome_msg)
+    )
+
+@handler.add(UnfollowEvent)
+def handle_unfollow(event):
+    print(event)
+@app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
-    signature = request.headers["X-Line-Signature"]
+    signature = request.headers['X-Line-Signature']
 
-    # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.info(f"Request body: {body}")
+    app.logger.info("Request body: " + body)
 
-    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
-        abort(400)
-    return "OK"
+        abort(100)
+
+    return 'OK'
 
 
-# 處理訊息
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    # Test
-    # message = TextSendMessage(text=event.message.text)
-    # line_bot_api.reply_message(event.reply_token, message)
-
-    message_text = str(event.message.text).lower()
-
-    if message_text == "@使用說明":
-        about_ius_event(event)
-    if message_text == "help":
-        Usage(event)
 
 
 if __name__ == "__main__":
